ef1b9f32757e5e14ca07e661e20e1a49c77f01d0
Chandana <rebbachandana54@gmail.com>
2026-01-30 11:39:46 +0000

    s

---
+++ local.tf
@@ -0,0 +1,20 @@
+locals{
+    common_tags={
+        project_name=var.project_name
+        terraform=true
+        environment=var.environment
+    }
+    common_name="${var.project_name}-${var.environment}"
+    ami_id=data.aws_ami.joindevops.id
+    sg_id=data.aws_ssm_parameter.sg_id.value
+    private_subnet_id=split(",",data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    private_subnet_ids=split(",",data.aws_ssm_parameter.private_subnet_ids.value)
+    vpc_id=data.aws_ssm_parameter.vpc_id.value
+    backend_alb_listener_arn=data.aws_ssm_parameter.backend_alb_listener.value
+    frontend_alb_listener_arn=data.aws_ssm_parameter.frontend_alb_listener.value
+    tg_port="${var.component}"=='frontend' ? 80:8080
+    heath_check_path="${var.component}"=='frontend' ? "/" : "/health"
+    listen_arn="${var.component}"== "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
+    host_context="${var.component}"== "frontend" ? "${var.project_name}-${var.environment}.${var.domain_name}":"${var.component}.backend-alb-${var.environment}.${var.domain_name}"
+    
+ }  
---
+++ var.tf
@@ -0,0 +1,21 @@
+variable "project_name"{
+    default="Roboshop"
+}
+variable "environment"{
+    default="Dev"
+}
+variable "instance_type"{
+    default="t3.micro"
+}
+variable "zone_id"{
+    default="Z04087883TWT696976M2R"
+}
+variable "domain_name"{
+    default="daws-86.shop"
+}
+variable "component"{
+   
+}
+variable "rule_priority"{
+    
+}
